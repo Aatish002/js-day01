bf8b6bf9aa44b452cc7518bdc58d1f823a5ede24
unknown <aatishbhatta02@gmail.com>
2024-08-06 09:23:48 +0545

    changes commit

---
+++ sandbox.js
@@ -0,0 +1,124 @@
+// input =[1,-3,2,4,-6,0,1,-1]
+
+// sumOfPostives
+// const array=[1,-3,2,4,-6,0,1,-1];
+
+// const arrlen = array.length
+
+// console.log(arrlen-1)
+
+// console.log(array[i])
+// let sum=0;
+// for (let i=0;i<array.length;i++){
+//     console.log(array[i])
+//     if (array[i]>0){
+//         console.log('positive numbers');
+//         sum=sum+array[i]
+//     }else{
+//         console.log('negative numbers');
+//     }
+
+    
+// }
+// console.log(sum);
+
+
+
+// output = sum
+
+// condtion ; postive of negative
+// loop; array
+
+
+
+
+// const arry =[1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// for (let i =0;i<arry.length;i++){
+//  console.log(arry[i])
+//   if (arry[i]>0 && arry[i] %2 == 0) {
+//     sum = sum +arry[i]  
+//   }
+// }
+// console.log('the sum of positive enen number:',sum);
+  
+
+
+// //define array
+
+// const arry=[1,2,3,4,5,6,7,8];
+// //intialize sum = 0
+// let sum = 0
+
+// //define check even sum function
+
+// function checkSumOfEvenNo(arry){
+//   for (let i = 0; i<arry.length;i++){
+//     console.log(arry[i]);
+  
+//     if (arry[i]>0 && arry[i] %2 == 0){
+//        sum = sum+arry[i];
+//     }
+  
+//   }
+//   return sum
+// }
+//  checkSumOfEvenNo(arry);
+//  console.log(sum);
+// let a = 1
+// function increase() {
+//   a++
+//   return a}
+
+//   increase()
+//   increase()
+//   increase()
+//   console.log(increase())
+
+//call function
+// console.log(
+//   checkSumOfEvenNo(arry)
+// );
+
+
+
+
+
+
+
+
+
+
+
+//Fabonacci unit 10 digii
+// const number= 11;
+// let a=0;
+// let b=1;
+
+// for (let i = 1; i <= number; i++) {
+//     console.log(a);
+//     c = a + b;
+//     a = b;
+//     b=c;
+
+
+//sum of first 20 numbers
+// function sumOfEvenNo(n){
+//     console.log(n*(n+1));
+// }
+// console.log(sumOfEvenNo(20));
+
+
+
+// let a = -Infinity
+// let b = -Infinity
+// let arr =[1,3,4,5,6,8];
+// for (i=0;i<=arr.length;i++){
+
+//  if (arr[i]>a){
+//    b=a
+//    a=arr[i]
+//   }
+//  }
+//  console.log(a,b)
+
